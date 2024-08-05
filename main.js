7f8b7beec802c7c4d99c7ba844ef2193fbcaa818
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 03:26:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// part 22// question 27// continue with question 25//2
+// ALien colour
+// Alien colour is green
+var alien_colour = "green";
+if (alien_colour === "green") {
+    console.log("player earned 5 point");
+}
+else if (alien_colour === "yellow") {
+    console.log("player earned 10 point");
+}
+else if (alien_colour === "red") {
+    console.log("player earned 15 point");
+}
+// alien colour is yellow
+alien_colour = "yellow";
+if (alien_colour === "green") {
+    console.log("player earned 5 point");
+}
+else if (alien_colour === "yellow") {
+    console.log("player earned 10 point");
+}
+else if (alien_colour === "red") {
+    console.log("player earned 15 point");
+}
+// alien colour is red
+alien_colour = "red";
+if (alien_colour === "green") {
+    console.log("player earned 5 point");
+}
+else if (alien_colour === "yellow") {
+    console.log("player earned 10 point");
+}
+else if (alien_colour === "red") {
+    console.log("player earned 15 point");
+}
